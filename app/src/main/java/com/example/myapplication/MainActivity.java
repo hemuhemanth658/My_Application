d037410623594b909e9c9d154167f6e1a4057133
hemuhemanth658 <59231711+hemuhemanth658@users.noreply.github.com>
2020-06-05 14:18:19 +0530

    Second Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
-package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,39 +7,73 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button hello = (Button)findViewById(R.id.button1);
-        final Button Button2= (Button)findViewById(R.id.button2);
-        final TextView t = (TextView)findViewById(R.id.text1);
-        final TextView t2=(TextView)findViewById(R.id.text2);
-        hello.setOnClickListener(new View.OnClickListener(){
+        final Button hello = (Button) findViewById(R.id.button1);
+        final Button Button2 = (Button) findViewById(R.id.button2);
+        final TextView t = (TextView) findViewById(R.id.text1);
+        final TextView t2 = (TextView) findViewById(R.id.text2);
+        hello.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                hello.setText((hello.getText() == "Show Text")?"Hide Text":"Show Text");
-                t.setVisibility((t.getVisibility() == View.VISIBLE)?View.INVISIBLE:View.VISIBLE);
+            public void onClick(View v) {
+                hello.setText((hello.getText() == "Show Text") ? "Hide Text" : "Show Text");
+                t.setVisibility((t.getVisibility() == View.VISIBLE) ? View.INVISIBLE : View.VISIBLE);
+
+            }
+        });
+
+        Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (t2.getVisibility() == View.INVISIBLE) {
+                    t2.setVisibility(View.VISIBLE);
+                    Button2.setEnabled(false);
+                } else {
+                    Button2.setEnabled(true);
                 }
-            });
-            t2.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    t2.setVisibility(View.INVISIBLE);
-                    return true;
-                }
-            });
-            Button2.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    Button2.setVisibility(View.VISIBLE);
-                    return true;
-                }
-            });
-        };
-    public void toast() {
-        Toast.makeText(this, "This is a toast", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        t2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Button2.setEnabled(true);
+                t2.setVisibility(View.INVISIBLE);
+                return true;
+            }
+        });
+
     }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "please don't kill me!", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "I am too young to kill", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.exitButton) {
+            onDestroy();
+        }
+    }
+
+
+//    public void toast() {
+//        Toast.makeText(this, "This is a toast", Toast.LENGTH_LONG).show();
+//
+//    }
 }
